9b4b793b7d79d5ad7c0af63fda23b01acabb29b1
Apurva N. Saraogi <Apurva N. Saraogi>
2017-09-29 19:51:08 +0530

    Projects and Story

--- home/models.py
+++ home/models.py
@@ -1,23 +1,58 @@
+from __future__ import unicode_literals
+from django import forms
+
 from django.db import models
+from wagtail.wagtailsnippets.models import register_snippet
 
 # Create your models here.
-from modelcluster.fields import ParentalKey
-
-from wagtail.wagtailcore.models import Page, Orderable
-from wagtail.wagtailcore.fields import RichTextField, StreamField
-from wagtail.wagtailcore import blocks
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel, MultiFieldPanel
-from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
 from wagtail.wagtailsearch import index
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtaildocs.blocks import DocumentChooserBlock
 from wagtail.wagtailembeds.blocks import EmbedBlock
+from wagtail.wagtailsnippets.blocks import SnippetChooserBlock
+from django.db.models import Q
+from story.models import StoryPage, StoryIndexPage
 
+from events.models import Events
+from django.db.models import Q
+class ImagecBlock(blocks.StructBlock):
+    image = ImageChooserBlock()
+    caption = blocks.TextBlock()
+
+    class Meta:
+        icon='cogs'
 
 class HomePage(Page):
+    slideshow = StreamField([
+        ('Image',ImagecBlock()),
 
+    ], blank=True)
+
+    AboutUs = RichTextField(blank=True)
+    Vision = RichTextField(blank=True)
+    facebook_link = models.CharField(max_length=200,default='')  
+    googleplus_link = models.CharField(blank=True,max_length=200)    
+    twitter_link = models.CharField(blank=True,max_length=200)   
+    youtube_link = models.CharField(blank=True, max_length=200)  
+    emailaddress = models.CharField(max_length=200,default='')
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(HomePage, self).get_context(request)
+        aevent = Events.objects.filter(Q(IsActive=1)).order_by('start')[:4]
+        context['aevent'] = aevent
+        return context
     # Database fields
-    about_us    = RichTextField('about_us', blank=True)
-    vision = RichTextField('vision', blank=True)
+
     feed_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -25,41 +60,18 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-    related_links = StreamField([
-        ('link', blocks.StructBlock([
-            ('name', blocks.CharBlock(max_length=255)),
-            ('url', blocks.URLBlock()),
-        ])),
-        ], blank = True)
-
-    carousel = StreamField([
-            #for images
-        ('image', ImageChooserBlock()),
-            # for quotes by people
-        ('quotation', blocks.StructBlock([
-            ('text', blocks.TextBlock()),
-            ('author', blocks.CharBlock()),
-        ])),
-        # for video embedding
-        ('video', EmbedBlock()),
-        ],
-        blank=True)
-
-    def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
-        context = super(HomePage, self).get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
-        return context
-
-    # Editor panels configuration
 
     content_panels = Page.content_panels + [
-        FieldPanel('about_us', classname="full"),
-        FieldPanel('vision', classname="full"),
-        StreamFieldPanel('carousel'),
-        StreamFieldPanel('related_links'),
-
+        MultiFieldPanel([
+            FieldPanel('facebook_link'),
+            FieldPanel('googleplus_link'),
+            FieldPanel('twitter_link'),
+            FieldPanel('youtube_link'),
+            FieldPanel('emailaddress'),
+        ], heading='Contact Links'),
+        StreamFieldPanel('slideshow'),
+        FieldPanel('AboutUs'),
+        FieldPanel('Vision'),
     ]
 
     promote_panels = [
