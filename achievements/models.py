688b47f7f174797fc0ff49921953467ad98f877e
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2018-01-17 14:10:41 +0530

    Improved readability of achievements app

--- achievements/models.py
+++ achievements/models.py
@@ -5,7 +5,7 @@ from modelcluster.fields import ParentalKey
 
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField, StreamField
-from wagtail.wagtailcore import blocks
+from wagtail.wagtailcore.blocks import StructBlock, TextBlock, TextBlock
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
@@ -13,48 +13,60 @@ from wagtail.wagtailsearch import index
 from wagtail.wagtailembeds.blocks import EmbedBlock
 
 
+
 class AchievementsIndexPage(Page):
-    intro = RichTextField(blank=True)
-    author = models.CharField(max_length=255, blank=True)
-    carousel = StreamField([
-        ('image', ImageChooserBlock()),
-        ('quotation', blocks.StructBlock([
-            ('text', blocks.TextBlock()),
-            ('author', blocks.CharBlock()),
-        ])),
-        ('video', EmbedBlock()),
-        ],
-        blank=True)
+	intro = RichTextField(blank=True)
+	
+	author = models.CharField(max_length=255, blank=True)
+	
+	carousel = StreamField(
+		[
+			('image', ImageChooserBlock()),
+			('quotation', blocks.StructBlock(
+				[
+					('text', blocks.TextBlock()),
+					('author', blocks.TextBlock()),
+				])
+			),
+			('video', EmbedBlock()),
+		],
+		blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full"),
-        FieldPanel('author'),
-        StreamFieldPanel('carousel'),
-    ]
+	content_panels = Page.content_panels + [
+		FieldPanel('intro', classname="full"),
+		FieldPanel('author'),
+		StreamFieldPanel('carousel'),
+	]
 
-    def get_context(self, request):
-        context = super(AchievementsIndexPage, self).get_context(request)
-        achievement = self.get_children().live().order_by('-first_published_at')
-        context['achievement'] = achievement
-        return context
-    subpage_types = ['achievements.AchievementsPage']
+	subpage_types = ['achievements.AchievementsPage']
+
+	def get_context(self, request):
+		context = super(AchievementsIndexPage, self).get_context(request)
+		achievement = self.get_children().live().order_by('-first_published_at')
+		context['achievement'] = achievement
+		return context
+
+
 
 class AchievementsPage(Page):
-    date = models.DateField("Post date")
-    competition_intro = models.CharField("About the Achievement",max_length=600)
-    description = RichTextField("Decription")
+	date = models.DateField("Post date")
 
-    carousel = StreamField([
-        ('image', ImageChooserBlock()),
-        ('video', EmbedBlock()),
-        ],
-        blank=True)
+	competition_intro = models.CharField("About the Achievement",max_length=600)
+	
+	description = RichTextField("Decription")
 
-    content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('competition_intro'),
-        FieldPanel('description', classname="full"),
-        StreamFieldPanel('carousel'),
+	carousel = StreamField(
+		[
+			('image', ImageChooserBlock()),
+			('video', EmbedBlock()),
+		],
+		blank=True)
 
-    ]
-    parent_page_types = ['achievements.AchievementsIndexPage']
+	content_panels = Page.content_panels + [
+		FieldPanel('date'),
+		FieldPanel('competition_intro'),
+		FieldPanel('description', classname="full"),
+		StreamFieldPanel('carousel'),
+	]
+	
+	parent_page_types = ['achievements.AchievementsIndexPage']
